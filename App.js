04479880c81f3bd3ae56c7c8d23ceb6380c73eac
Badal Agarwal <63859255+badal-ag@users.noreply.github.com>
2022-07-11 22:47:56 +0530

    Basic Layout changed

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>FaceDuo - A React Native based Video Calling App</Text>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
